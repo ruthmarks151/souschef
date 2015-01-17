9041a21706b4a9f910d0c2be35b06f6e579e69da
Arya Solhi <aryasolhi@gmail.com>
2015-01-17 10:11:48 -0500

    Added angular post

--- static/app.js
+++ static/app.js
@@ -1,7 +1,9 @@
 var App = angular.module('SousChef', []);
 
-App.controller('ContentController', ['$scope',function($scope){
+App.controller('ContentController', ['$scope','$http',function($scope, $http){
     $scope.submitSearch = function (){
-        
+        $http.post('/recipe/get/all',{search:$scope.searchValue}).success(function(data){
+            $scope.searchOptions = data;
+        })
     }
 }])
