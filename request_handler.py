0ceb2368c49c34dfbc34ce053ede528cf88bb3f2
Kestin Goforth <kgoforth1503@gmail.com>
2015-01-17 15:38:33 -0500

    Added ingredient parser, removed unsused send_timer

--- request_handler.py
+++ request_handler.py
@@ -17,10 +17,10 @@ class request_handler:
             'query_temp': self.query_temp,
             'query_time': self.query_time,
             'convert_unit': self.convert_unit,
-            'send_timer': self.send_timer,
             'get_current_step': self.get_current_step,
             'next_step': self.next_step,
             'previous_step': self.previous_step,
+            'ingredients': self.ingredient
         }
 
         return functions[request["intent"]](request)
@@ -31,18 +31,34 @@ class request_handler:
         return self.recipe.get_current_time_text()
     def convert_unit(self,request):
         parse_unit_converstion(request)
-    def send_timer(self,request):
-        pass
     def get_current_step(self,request):
         return self.recipe.get_current_step().get_step_text()
     def next_step(self,request):
         return self.recipe.get_next_step().get_step_text()
     def previous_step(self,request):
-        return self.recipe.get_previous_step().get_step_text()
+        return self.recipe.get_previous_step().get_step_text
+    def ingredients(self, request):
+        return find_best_ingredient_amount(request)
 
 
-    def parse_unit_conversion(self, raw_statement):
-        words = raw_statement.split()
+    def find_best_ingredient_amount(phrase):
+        words_in_phrase = phrase.split()
+        ingredients = recipe.get_ingredients()
+        highest_matching = 0
+        best_match = None
+        for ingred in ingredients:
+            matches = 0
+            for i in ingred.getName().split():
+                for word in words_in_phrase:
+                    if(word.lower() == i.lower()):
+                        matches++;
+            if matches > highest_matching:
+                highest_matching = matches
+                best_match = ingred
+        return "You need " + ingred.get_amount_string() + " " + ingred.get_unts() + " of " + ingred.getName()
+
+    def parse_unit_conversion(self, phrase):
+        words = phrase.split()
         numbers = []
         units = []
         for i in range(len(words)):
