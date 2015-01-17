1308dd08efef1341c5ae0d5bfc974ed218589b07
Kestin Goforth <kgoforth1503@gmail.com>
2015-01-17 15:05:00 -0500

    Added unit parsing from speech

--- request_handler.py
+++ request_handler.py
@@ -30,7 +30,7 @@ class request_handler:
     def query_time(self,request):
         return self.recipe.get_current_time_text()
     def convert_unit(self,request):
-        pass
+        parse_unit_converstion(request)
     def send_timer(self,request):
         pass
     def get_current_step(self,request):
@@ -39,3 +39,22 @@ class request_handler:
         return self.recipe.get_next_step().get_step_text()
     def previous_step(self,request):
         return self.recipe.get_previous_step().get_step_text()
+
+
+    def parse_unit_conversion(self, raw_statement):
+        words = raw_statement.split()
+        numbers = []
+        units = []
+        for i in range(len(words)):
+            try:
+                val = eval(words[i])
+                if type(val) == int or type(val) == float:
+                    numbers.append(val)
+                    units.append(words[i + 1])
+            except:
+                if words[i] == 'to' or words[i] == 'in':
+                    units.append(words[i + 1])
+                continue
+
+        final_string = str(numbers[0]) + " " + str(units[0]) + " is equal to " + str(Convert.convert_unit(numbers[0], units[0], units[1]))
+        return final_string
