66f4b24767c0116be274c76290b09a88843c0332
Kestin Goforth <kgoforth1503@gmail.com>
2015-01-17 00:37:19 -0500

    unfucked stuff up

--- index.py
+++ index.py
@@ -6,10 +6,6 @@ app = Flask(__name__, static_url_path='')
 def root():
     return app.send_static_file('index.html')
 
-<<<<<<< HEAD
-if __name__ == "__main__":
-    app.run(host='104.236.40.40', port=80, debug=true)                                    
-=======
 import os
 @app.route('/static/<path:path>')
 def static_proxy(path):
@@ -18,4 +14,3 @@ def static_proxy(path):
 
 if __name__ == '__main__':  # pragma: no cover
     app.run(host='0.0.0.0', port=5000)
->>>>>>> 404a5832de766d8c9ff4a72d9f6adbf7a2a2f3d3
