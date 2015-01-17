eb19e7257d19529614cf553b06d11cce99df746b
Kestin Goforth <kgoforth1503@gmail.com>
2015-01-16 21:41:42 -0500

    Cleaned up code

--- Kestin/RecipeGetter.py
+++ Kestin/RecipeGetter.py
@@ -6,31 +6,27 @@ RETRIES = 0
 
 client = Client(api_id = '0ad05d37', api_key = 'b616f863887a7d2cb6d4baf30cd3cbe2', timeout = TIMEOUT, retries = RETRIES)
 
-def getFirstRecipeIdFromSearch(search_params):
-    results = client.search(**search_params)
-    match = results.matches[0]
-    print match
-    return match.id
-
+allowed_recipe_sources = ["Pillsbury Brand", "Betty Crocker", "Tablespoon"]
 def getFirstUseableRecipeId(params):
     results = client.search(**params)
     matches = results.matches
     for match in matches:
-        if match.sourceDisplayName in allowed_source:
+        if match.sourceDisplayName in allowed_recipe_sources:
         return match
 
 def getRecipeFromId(id):
     return client.recipe(id)
 
-allowed_source = ["Pillsbury Brand", "Betty Crocker", "Tablespoon"]
-
 def getSourceFromRecipe(recipe):
     url = recipe.source.sourceRecipeUrl
     return url
 
-params = {
+search_params = {
     'q': "bacon",
     'start': 0,
     'maxResults': 100,
     'requirePicutres': True
 }
+
+def getSteps():
+    return 4
