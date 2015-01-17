2afe84759b7bd4ae8d7e27c386cbc47bc83b1faf
Ryan Marks <Ryan@Sigmabot.com>
2015-01-17 09:07:06 -0500

    Added request handler object

--- index.py
+++ index.py
@@ -1,16 +1,26 @@
 from flask import Flask, request
+import sys
+import os
+from request_handler import request_handler
+sys.path.insert(0, './recipes/')
+import RecipeGetter
+
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
+
+def create_recipe(search):
+    return request_handler(RecipeGetter.getRecipe(search))
+
+rh = create_recipe("bacon")
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
 
-import os
 @app.route('/static/<path:path>')
 def static_proxy(path):
     # send_static_file will guess the correct MIME type
     return app.send_static_file(os.path.join('static', path))
 
 if __name__ == '__main__':  # pragma: no cover
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
---
+++ request_handler.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.insert(0, './recipes/')
+import Recipe
+import convert
+
+class request_handler:
+
+    def __init__(self,recipe):
+        self.recipe = recipe
+
+    def handle_request(self,request):
+        functions = {
+        'query_temp': self.query_temp,
+        'query_time': self.query_time,
+        'convert_unit': self.convert_unit,
+        'send_timer': self.send_timer,
+        'get_current_step': self.get_current_step,
+        'next_step': self.next_step,
+        'previous_step': self.previous_step,
+        'ingredients': self.ingredients
+        }
+
+        return functions[request["intent"]](request)
+
+    def query_temp(self,request):
+        return self.recipe.get_current_temp_text()
+    def query_time(self,request):
+        return self.recipe.get_current_time_text()
+    def convert_unit(self,request):
+        pass
+    def send_timer(self,request):
+        pass
+    def get_current_step(self,request):
+        return self.recipe.get_current_step().get_step_text()
+    def next_step(self,request):
+        return self.recipe.get_next_step().get_step_text()
+    def previous_step(self,request):
+        return self.recipe.get_previous_step().get_step_text()
+    def ingredients(self,request):
+        pass
