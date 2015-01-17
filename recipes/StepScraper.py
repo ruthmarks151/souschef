e4e6440c8b27835ea6322ddbf7a579c6e03b3247
Kestin Goforth <kgoforth1503@gmail.com>
2015-01-16 21:46:15 -0500

    Re-structured functions

--- recipes/StepScraper.py
+++ recipes/StepScraper.py
@@ -1,13 +1,13 @@
 from webscraping import download, xpath
 
-def getExternalRecipeUrl(url):
+def getExternalRecipeUrlFromYummlyUrl(url):
     dl = download.Download()
     html = dl.get(url)
     external_url = xpath.search(html, '//div[@class="external-directions flex-box"]//button/@link')
     actual_url = "http://www.yummly.com" + (external_url[0])
     return actual_url
 
-def getSourceUrlFromYummlyUrl(url):
+def getSourceUrlFromExternalUrl(url):
     dl = download.Download()
     html = dl.get(url)
     source_url = xpath.search(html, '//td[@class="close"]//a/@href')
@@ -19,15 +19,16 @@ def getStepsFromSourceUrl(url):
     steps = xpath.search(html, '//span[@class="recipePartStepDescription"]')
     return steps
 
-def getStepsFromYummlyUrl(yummly_url):
-    external_url = getExternalRecipeUrl(yummly_url)
-    source_url = getSourceUrlFromYummlyUrl(external_url)
-    steps = getStepsFromSourceUrl(source_url)
-    return steps
-
 def getUniqueSteps(steps):
     unique_steps = []
     for step in steps:
         if step not in unique_steps:
             unique_steps.append(step)
     return unique_steps
+
+def getStepsFromYummlyUrl(yummly_url):
+    external_url = getExternalRecipeUrlFromYummlyUrl(yummly_url)
+    source_url = getSourceUrlFromExternalUrl(external_url)
+    steps = getStepsFromSourceUrl(source_url)
+    steps = getUniqueSteps(steps);
+    return steps
