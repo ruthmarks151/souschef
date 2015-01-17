9db490118d3ecff1e5031d001760b3029a97a054
Kestin Goforth <kgoforth1503@gmail.com>
2015-01-16 21:38:54 -0500

    Currently works for Pillsbury, Betty Crocker, and Tablespoon

--- Kestin/RecipeGetter.py
+++ Kestin/RecipeGetter.py
@@ -6,48 +6,31 @@ RETRIES = 0
 
 client = Client(api_id = '0ad05d37', api_key = 'b616f863887a7d2cb6d4baf30cd3cbe2', timeout = TIMEOUT, retries = RETRIES)
 
-def getFirstRecipeIdFromSearch(search_terms):
-    results = client.search(search_terms)
+def getFirstRecipeIdFromSearch(search_params):
+    results = client.search(**search_params)
     match = results.matches[0]
+    print match
     return match.id
 
-def listRecipesFromSearch(search_terms):
-    results = client.search(search_terms, maxResults=1)
+def getFirstUseableRecipeId(params):
+    results = client.search(**params)
+    matches = results.matches
+    for match in matches:
+        if match.sourceDisplayName in allowed_source:
+        return match
 
-    print 'Total Matches:', results.totalMatchCount
-    for match in results.matches:
-        try:
-            print 'Recipe ID:', match.id
-            print 'Recipe:', match.recipeName
-            print 'Rating:', match.rating
-            print 'Total Time (mins):', (match.totalTimeInSeconds / 60.0)
-            print '----------------------------------------------------'
-        except:
-            print '==ERROR=='
+def getRecipeFromId(id):
+    return client.recipe(id)
 
-def getRecipeInfo(recipe_id):
-    return client.recipe(recipe_id)
-
-def printRecipeInfo(recipe):
-    print ""
-    print 'Recipe ID:', recipe.id
-    print 'Recipe:', recipe.name
-    print 'Rating:', recipe.rating
-    print 'Total Time:', recipe.totalTime
-    print 'Yields:', recipe.yields
-    print 'Ingredients:'
-    for ingred in recipe.ingredientLines:
-        print ingred
-    print ""
+allowed_source = ["Pillsbury Brand", "Betty Crocker", "Tablespoon"]
 
 def getSourceFromRecipe(recipe):
     url = recipe.source.sourceRecipeUrl
     return url
 
-a = getFirstRecipeIdFromSearch('bacon')
-b = getRecipeInfo(a)
-c = getSourceFromRecipe(b)
-d = StepScraper.getStepsFromYummlyUrl(c)
-e = StepScraper.getUniqueSteps(d)
-print e
-printRecipeInfo(b)
+params = {
+    'q': "bacon",
+    'start': 0,
+    'maxResults': 100,
+    'requirePicutres': True
+}
