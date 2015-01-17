2600be7d28da8e1339c5db3193ae2f578427a788
Arya Solhi <aryasolhi@gmail.com>
2015-01-17 04:48:59 -0500

    trying to serve static html

--- index.py
+++ index.py
@@ -3,9 +3,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def hello():
-    return 'works'
-
-url_for('public_html', filename='index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='104.236.40.40', port=80, debug=true)                                     
